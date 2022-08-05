4169c47be7a98f822adb8e9748dfef7d71078f51
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-08-05 10:41:35 -0500

    add arrays for names, nouns, verbs, & adjectives

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const names = [
+    'Beyonce', 'Simon Cowell', 'Uhtred son of Uhtred', 'Nancy Pelosi', 'Henry Cisneros'
+]
+
+const nouns = [
+    'jackal', 'sandwich', 'scooter', 'crayon', 'microphone', 'monkey', 'fish', 'motorcycle', 'lizard', 'xylophone'
+]
+
+const verbs = [
+    'announce', 'bake', 'collect', 'dance', 'exercise', 'fly', 'guess', 'help', 'jump', 'laugh'
+]
+
+const adjectives = [
+    'abrasive', 'greasy', 'reprehensible', 'harmless', 'rhythmic', 'scrappy', 'imaginary', 'biased', 'charming', 'tepid'
+]
