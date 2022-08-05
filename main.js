d4f53330d6459671ec9fdf63a9b6a379112c9f3f
Matthew-Mays <matthew.c.mays96@gmail.com>
2022-08-05 10:28:58 -0500

    add function to select random array value. Add adverbs and pronouns array.

---
+++ main.js
@@ -0,0 +1,11 @@
+const randomArrayValue = array => {
+    let arrayIndex = Math.floor(Math.random() * array.length);
+    return array[arrayIndex];
+}
+
+const adverbs = [
+    'bravely', 'cheerfully', 'deftly', 'gracefully', 'warmly', 'lazily', 'rudely', 'wearily', 'quickly', 'leisurely'];
+
+const pronouns = [
+    'he', 'she', 'they', 'we', 'it', 'I', 'you'];
+
